1b285f30ff2ae793859c1ecd8c245f060d3b3ead
Do1ore <100031663+Do1ore@users.noreply.github.com>
2023-08-01 00:17:42 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SignalRStringService} from "./services/signal-r-string.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(public signalRService: SignalRStringService) {
+  }
+
   title = 'StringEncoderWebUI';
+
+  inputString: string = "";
+  encodedString: string = "";
+
+  async ngOnInit() {
+    this.signalRService.getHubConnection();
+
+    //start listener
+    this.signalRService.signalRConnect$.subscribe(() => {
+      console.log('listener started👍')
+      this.signalRService.encodeStringRequestListener();
+
+      this.signalRService.encodedStringSubject.asObservable().subscribe((symbol) => {
+        this.encodedString += symbol;
+      })
+    })
+  }
+
+  async encodeString() {
+    console.log('argument: ', this.inputString);
+    this.encodedString = '';
+    if (this.signalRService.hubConnection?.state != 'Connected') {
+      console.log('Unconnected!!!')
+      return;
+    }
+    await this.signalRService.encodeStringRequest(this.inputString)
+
+
+  }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule, OnInit} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {SignalRStringService} from "./services/signal-r-string.service";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
+
